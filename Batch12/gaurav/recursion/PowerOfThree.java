69e6ef567197a468ed9458116df8e17e4c77b2db
Gaurav <gauravmi@users.noreply.github.com>
2022-04-13 04:15:50 -0700

    Create PowerOfThree.java

---
+++ Batch12/gaurav/recursion/PowerOfThree.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/power-of-three/submissions/
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if( n == 0 ) return false;
+        if( n % 3 == 0) return isPowerOfThree( n / 3);
+        if( n == 1 ) return true;
+        return false;
+    }
+}
