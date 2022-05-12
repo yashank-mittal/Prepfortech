ee645458a46f5fba457fb2557d896913dc951cf1
musadhiek <55571996+musadhiek@users.noreply.github.com>
2022-05-12 07:39:58 +0530

    Create max_depth_dft.py

---
+++ Batch12/musadhiek/trees/max_depth_dft.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        return max(self.depth_count(root.left,count=1),self.depth_count(root.right,count=1))
+    
+    def depth_count(self,root,count):
+        if root == None:
+            return count
+        return max(self.depth_count(root.left,count+1),self.depth_count(root.right,count+1))
+        
+
+ class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
